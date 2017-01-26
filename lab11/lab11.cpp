80a4bc3bed73343ccba120af69d64fd93ca283b8
Tanner Little <tlittle006@student.butte.edu>
2017-01-26 23:03:22 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,15 @@
+/* Start the program.
+The computer must choose a number between 0 and 55 including 0 and 55.
+The randomly chosen number is stored and the computer remembers it.
+The program asks for user input to type a number between 0 and 55. Then press ENTER.
+The computer compares the input number to the stored number.
+If input number is lower than stored number, output "higher".
+If input number is higher than stored number, output "lower".
+Show score counter that begins at 10.
+If input number differs from stored number in any way, deduct 1 from score counter.
+If input number is the same as stored number, output "You Win!"
+If score counter reaches 0 or the output "You Win!" is displayed, output "You Lose. Would you like to try again?"
+User is asked to input "y" for yes or "n" for no.
+If "y" then then the program starts over.
+If "n" then the program ends.
+End program */
