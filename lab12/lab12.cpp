230ce6b28a1e814ee48f250a255014db0af35e20
Tanner Little <tlittle006@student.butte.edu>
2017-01-31 22:40:17 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,66 @@
+// Created By: Tanner Little
+// Created On: 1/31/2017
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"DMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM8"<<endl;
+    cout<<"8MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"<<endl;
+    cout<<"OMM .                      ....             ...                    .. .     ...     .MMD"<<endl;
+    cout<<"ZMM .                      .MM.            .MM.                    NM       MM.       MM"<<endl;
+    cout<<"7MM .                =8    .MM     .:7.    .MM.            ...     MM..  . MMM.       MM"<<endl;
+    cout<<"?MM..    ......      MM=   .MM,     MM$.   .MM.    ...     MM..   DMM    ,MM$        .MM"<<endl;
+    cout<<"~MM..    +MMM=..     NMM.   DM,     NM8    .MM.    MM..    MM.   .MM.   ,MM+          MM"<<endl;
+    cout<<".MM.       .MMMM...      . ....             MM.    MM8.    $M  .  ...  :MM,   .....  .MM"<<endl;
+    cout<<".MM,        ..MMMM.      ~MMMMMMM8 .        MM,    MMM.    ..OMMMM8..   ,..  .+MMM.  .MM"<<endl;
+    cout<<" MM,.         ..MM. . IMMMMMMMMMMMMMM..    .MM=      ..  .MMMMMMMMMMMMM..    MMM~    .MM"<<endl;
+    cout<<" MM:               .7MMMMM8ZZZZZZMMMMMM..  .MM=      . .MMMMMOZZZZZNMMMMM=. . ...    .MM"<<endl;
+    cout<<" MM:     ... .   .,MMMMOZZZZZZZZZZZZ8MMMN .          .MMMMOZZZZZZZZZZZZMMMM?.       . MM"<<endl;
+    cout<<" MM~     MMMMM   MMMM8ZZZZZZZZZZZZZZZZMMMM.         .MMMMZZZZZZZZZZZZZZZZMMMN   .MMM..MM"<<endl;
+    cout<<" MM~      ... ..MMMMZZZZZZZZZZZZZZZZZZZMMMM..      $MMMZZZZZZZZZZZZZZZZZZZMMM,. MMM. .MM"<<endl;
+    cout<<" MM=  . .      =MMM$ZZZZZZZZZZZZZZZZZZZZMMMM...   NMMMZZZZZZZZZZZZZZZZZZZZZMMM  . .  =MM"<<endl;
+    cout<<" MM++MMMMM?..  MMMOZZZZZZZZZZZZZZZZZZZZZZMMMM . .NMMMZZZZZZZZZZZZZZZZZZZZZZDMM,.     ZMM"<<endl;
+    cout<<" MM+:?NMMMMM.  MMMZZZZZZZZZZZZZZZZZZZZZZZZMMMM. MMMMZZZZZZZZZZZZZZZZZZZZZZZOMMM .... MMM"<<endl;
+    cout<<" MM+.....  ..  MMMZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZMMM. MMM MMM"<<endl;
+    cout<<" MM+.          MMMZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZMMM    ..DMM"<<endl;
+    cout<<" MM?.  ..,=..  MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZMMM.     ZMM"<<endl;
+    cout<<" MM?   MMMMM   DMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMM   ...=MM"<<endl;
+    cout<<" MM?.  ...     .MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZDMMN ?MM,..MM"<<endl;
+    cout<<" MM?            MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMM. MMMMM.MM"<<endl;
+    cout<<" MM+....ZMMMMM..:MMNZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZOMMM . .    MM"<<endl;
+    cout<<" MM+.:MMMMMMMN   MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMMMMM.   .MM"<<endl;
+    cout<<" MM=.      .... ..MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZNMMMMMMMMM.  .MM"<<endl;
+    cout<<" MM=       .?+.. .DMMNZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMM.  MMM    MM"<<endl;
+    cout<<" MM~     ZMMMMMM.. MMMOZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMM.  .MMZ   .MM"<<endl;
+    cout<<" MM:.   MMMN. MMM8. MMMOZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMM  .MMM .  .MM"<<endl;
+    cout<<" MM,   .MMM.   MMM   MMMOZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZOMMMMM . ~MM~   .MM"<<endl;
+    cout<<" MM..   MM...  ZMM.  .MMMOZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ8MMMMMM . .MMM   .MM"<<endl;
+    cout<<" MM. ..MMM.    MMM.   .MMMDZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMM.MMM~.  .MMM ..MM"<<endl;
+    cout<<" MM..MMMM.    MMM$ .... MMMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMM..+MMM   ..MMM,.MM"<<endl;
+    cout<<" MMMMMM.   ..MMM  :MMO.  8MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZNMMM.  ..MMMM .  .MMMMMM"<<endl;
+    cout<<".MMMMI.  ..DMMM  MMMO.... ~MMMZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZMMMM. MM7..MMMI     MMMMM"<<endl;
+    cout<<".MMM..   .MMMM.. .....MMM.  MMMMZZZZZZZZZZZZZZZZZODZZZZZZZZZZOMMMD.. IMMM  MMM     .MMMM"<<endl;
+    cout<<",MM.   .~MMM.      .MMMM~   .MMMMZZZZZZZZZNMMZZMMMMMMMZZZZZZMMMM  MM. .MM?  MMMD.   .MMM"<<endl;
+    cout<<":MM.   OMM8..     DMMM ...MM. .MMMMZZZZMMMMMMMMM$  .MMMZZZDMMM:. .,M=.     ,MMMM.  .MMMM"<<endl;
+    cout<<"~MM.   .MMMM.      O.....MMM   .MMMM$ZMMMM...MMMM.. MMMZZMMMM. .MMMMMM$...MMMM.. .,MMMMM"<<endl;
+    cout<<"=MM.    ..MMM+.    . IMNI..     .=MMMMMMM..   MMM..?MMMNMMM,..MMMMMMMMMMMMMM     :MMM.MM"<<endl;
+    cout<<"+MMM.      ZMMM ..7MMMMMMMMM. .  ..MMMMM,. . DMMM..MMMMMMM. IMMM, . ..MMMM,..   $MMM..MM"<<endl;
+    cout<<"?MMMMM. .  . NMMMMMM$..  .MMMM.. MM..MMM. .MMMM+. ?MMMMM. ..MMM.      .MMM.  . MMMD  .MM"<<endl;
+    cout<<"?MM.MMM7..     MMMM .      .MMM.MMM.NMM...~MMMM.. MMMM~....7MM7        $MM.  .MMM,.  .MM"<<endl;
+    cout<<"IMM..OMMM......MMM.         MMM .. .MMN  .MMMMM ..MMM..MMM.=MM~         MM?  :MM,.   ,MM"<<endl;
+    cout<<"7MM.. .MMMI...$MM..         IMM=:. OMM..  MMMMM ..MMM . MM..MMM..      .MM,   MMM .  ~MM"<<endl;
+    cout<<"$MM..  .:MMI.. MMM.        .MMMMMMMMMM. . MMMMM.. MMM. .?MMMMMMM7.     ,DI.   .MM+   =MM"<<endl;
+    cout<<"ZMM..   ?MM.   MMMM..      .MM= . .      DMMMMM...MMMMMMMMMMMMMMMMM.           MMM.  =MM"<<endl;
+    cout<<"OMM..   MMM..   IMMM.      MMM     ...   MMMMMM . MMMMO~  .     MM,            MMM   ~MM"<<endl;
+    cout<<"8MM..   MMM.     ....       .      =ZMMMMMM.MMM .....  ...     . ..            $MM   ,MM"<<endl;
+    cout<<"8MM..   MMM..                .MMMMMMMMMMMM..NMM .                              +MM   .MM"<<endl;
+    cout<<"DMM..  .MMM..                .MM.    .     ..MMMMMMMMMMMMMMMMMM..              ~MM.   MM"<<endl;
+    cout<<"8MM..  .MMM.                 .MM$            MMMMMMMMMMMMMMMMMMD.              ~MM   .MM"<<endl;
+    cout<<"7MM..  .,MM.                  MMM.                           MMM.              +MM.  .MM"<<endl;
+    cout<<"+MM..   .MMM                  MMM                            $MM.              IMM.   MM"<<endl;
+    cout<<",MMMMI...MMM.. ..  .... ..,~?ZMMMMMMMNNNNDDNNMMMMMMMMMMMMMMMMMMMMMM8$77$Z8NMMMMNMMMMMMMM"<<endl;
+    cout<<" MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"<<endl;
+    
+}
