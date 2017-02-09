a77b4c6af67de4920e20c8c3180f96d2da9cfe7b
Tanner Little <tlittle006@student.butte.edu>
2017-02-09 22:56:02 +0000

    Lab1.5 submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,49 @@
+// Created By: Tanner Little
+// Created On: 2/7/2017
+// 1.5 Lab - Output a Mad Lib using assigned variables
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string noun1 = "Stomach";
+    string exclamation = "Wazzoo";
+    string verbIng = "chafing";
+    string nameRandom = "Xavier";
+    string adjective1 = "moist";
+    string noun4 = "tooth";
+    string personRm = "Tanner";
+    int number1 = 47;
+    double number2 = 4.5;
+    string nounPl1 = "monkeys";
+    string noun2 = "tupperware";
+    string adjective2 = "slippery";
+    string color = "cyan";
+    string verb = "puke";
+    string typeOfFood = "Carnitas";
+    string nameRandom2 = "Christopher";
+    string partOfBody = "booty";
+    string nounPl2 = "Toes";
+    string noun3 = "Blueprint";
+    
+    cout<<"            The 89th Annual "<<noun1<<" Awards!!!"<<endl;
+    cout<<exclamation<<"! It's the awards show you've been "<<verbIng<<" for all year: "<<endl;
+    cout<<"the "<<nameRandom<<"s! There are many "<<adjective1<<" reasons to watch this "<<endl;
+    cout<<"year's "<<noun4<<". Here are a few: "<<endl;
+    cout<<" @ "<<personRm<<" is hosting so you're guarenteed at least "<<number1<<endl;
+    cout<<"   good laughs."<<endl;
+    cout<<" @ You invested "<<number2<<" dollars to watch all the "<<nounPl1<<endl;
+    cout<<"   nominated for Best Picture. That's money you could have spent on a "<<endl;
+    cout<<"   brand-new "<<noun2<<"!"<<endl;
+    cout<<" @ To admire all the "<<adjective2<<" dresses on the "<<color<<endl;
+    cout<<"   carpet and "<<verb<<" at all the fashion disasters!"<<endl;
+    cout<<" @ You need an excuse to make "<<typeOfFood<<" Pie for your annual "<<endl;
+    cout<<"   "<<nameRandom2<<"s themed party."<<endl;
+    cout<<" @ You might lose your "<<partOfBody<<" if you watch one more rerun "<<endl;
+    cout<<"   of Chopped and Diners, "<<nounPl2<<", and Dives on the "<<endl;
+    cout<<"   "<<noun3<<" Network."<<endl;
+
+}
