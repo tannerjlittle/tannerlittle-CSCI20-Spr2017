3e12771f0af808176a1ca4a9062c4e8ea5192256
Tanner Little <tlittle006@student.butte.edu>
2017-04-04 21:33:48 +0000

    Lab3.4 submission

---
+++ lab34/lab34.cpp
@@ -0,0 +1,31 @@
+// Created By: Tanner Little
+// Created On: 4/4/2017
+// Lab 3.4 - Debugging programs
+
+/***************************************************
+Sum of numbers 
+Write a program that reads in positive integers from the user, each separated by a space, 
+and the whole terminated by a negative number (e.g. 3 22 10 -2),and outputs the sum 
+of the numbers entered (notincluding the terminated negative number!).
+***************************************************/
+#include <iostream>
+using namespace std; 
+int main(){
+    // initialization
+    int num = 0;
+    int sum = 0;
+    cout<<"Enter numbers separated by spaces and terminated with a negative number."
+        << endl;
+     // read first number
+    cin >> num;
+        while (num >= 0) {
+           // add in to the sum
+           sum = sum + num;
+            // read in next number (getting ready for next loop test)
+           cin>>num;
+        }
+            cout<<"Sum was "
+                <<sum
+                <<endl;
+            return 0;
+}
