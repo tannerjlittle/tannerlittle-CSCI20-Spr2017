4788a7fd500285c757ee9b9f81a093f00fb6e89b
Tanner Little <tlittle006@student.butte.edu>
2017-05-09 21:55:14 +0000

    Lab52 submission

--- lab52/class.h
+++ lab52/class.h
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-/*
- *  This class is to show how to
- *  break our class into an interface and implementation
- *  file. It will set a member variable to 0 on initialization
- *  and output it using the Output() function.
- */
 class MyClass {
  public:
   /*
