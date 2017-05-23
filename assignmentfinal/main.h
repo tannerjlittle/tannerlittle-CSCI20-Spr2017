366a8bd08360320de2c7dc53d4bbfcee2646cbd7
Tanner Little <tlittle006@student.butte.edu>
2017-05-23 18:56:53 +0000

    5.0 Final Assignment submission

--- assignmentfinal/main.h
+++ assignmentfinal/main.h
@@ -2,7 +2,7 @@
 // Created On: 5/11/2017
 /* Final Assignment - A family tree generator with a person class, a group of functions, 
    file I/O, arrays, lots of pointers, and a makefile */
-
+// Idea from GitHub user vogosvagen
 
 #include <iostream>
 #include <string>
